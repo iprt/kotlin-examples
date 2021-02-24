d06b301be27e5f0e4b0c55adcb50d42ab7e903ed
zhuzhenjie <mimotronik@gmail.com>
2021-02-24 10:53:49 +0800

    controller 层异常处理

--- src/main/kotlin/org/example/hello/config/ControllerExceptionHandler.kt
+++ src/main/kotlin/org/example/hello/config/ControllerExceptionHandler.kt
@@ -1,20 +1,28 @@
 package org.example.hello.config
 
 import org.apache.commons.lang3.StringUtils
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 /**
+ * controller 层的异常处理
+ *
  * @author winterfell
  * @since 2021/1/6
  */
 @RestControllerAdvice
 class ControllerExceptionHandler {
+
     private val log = getLogger(this::class.java)
 
     @ExceptionHandler(Exception::class)
-    fun handleException(exception: Exception): String? {
+    fun handleException(exception: Exception): ResponseEntity<String> {
         log.error(StringUtils.EMPTY, exception)
-        return exception.message
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(exception.message ?: StringUtils.EMPTY)
     }
 }
