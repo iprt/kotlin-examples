b922a111ec70314c800619562efd4b6f0a576303
zhenjie zhu <devops@kubectl.net>
2024-04-15 11:55:47 +0800

    Refactor init.sql for style improvements
    The changes involve consistent usage of lowercase, adding spaces for readability in SQL commands, and reformatting the data insertion lines for clarity. The updated formatting enhances readability and maintainability of the code.

--- .sql/init.sql
+++ .sql/init.sql
@@ -2,20 +2,22 @@ drop table if exists student;
 
 create table student
 (
-    `id`   BIGINT PRIMARY KEY COMMENT '主键ID',
-    `name` VARCHAR(32) NOT NULL COMMENT '名称',
-    `info` json        NOT NULL COMMENT '信息'
-)engine=InnoDB row_format=dynamic;
+    `id`   BIGINT primary key comment '主键ID',
+    `name` VARCHAR(32) not null comment '名称',
+    `info` json        not null comment '信息'
+) engine = InnoDB
+  row_format = dynamic;
 
 insert into student (`id`, `name`, `info`)
-values (1, 'hello', '{
-  "father": "dad",
-  "mother": "mom"
-}'),
-       (2, 'kotlin', '{
-         "lgtd": 123.10086,
-         "lttd": 30.213
-       }');
+values
+    (1, 'hello', '{
+      "father": "dad",
+      "mother": "mom"
+    }'),
+    (2, 'kotlin', '{
+      "lgtd": 123.10086,
+      "lttd": 30.213
+    }');
 
 
 
@@ -26,7 +28,8 @@ create table benchmarks_auto
     `id`   BIGINT      not null auto_increment comment 'id',
     `data` varchar(50) not null comment 'data',
     primary key (`id`)
-)engine=InnoDB row_format=dynamic;
+) engine = InnoDB
+  row_format = dynamic;
 
 
 drop table if exists benchmarks_flow;
@@ -36,4 +39,5 @@ create table benchmarks_flow
     `id`   BIGINT      not null comment 'id',
     `data` varchar(50) not null comment 'data',
     primary key (`id`)
-)engine=InnoDB row_format=dynamic;
+) engine = InnoDB
+  row_format = dynamic;
