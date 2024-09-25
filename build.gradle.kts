93a2e7d508eda5a60876d4a5183fb41004c286ee
zhenjie zhu <tech@intellij.io>
2024-09-25 16:07:58 +0800

    Update Spring Boot version and increase log file size limits
    Upgraded Spring Boot from version 3.3.2 to 3.3.4 in build.gradle.kts to utilize the latest features and fixes. Also, increased the log file size limits from 20MB to 100MB in logback-spring.xml to handle larger log files efficiently.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
-    id("org.springframework.boot") version "3.3.2"
+    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
 }
 
