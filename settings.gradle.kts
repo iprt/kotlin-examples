60f1ee2114450223fe9acc143078e67d60dad5b4
zhenjie zhu <tech@intellij.io>
2024-12-04 19:08:12 +0800

    Update Gradle repositories for build process.
    This commit adds Aliyun's and Tencent's mirrors to the Gradle settings and wrapper, aiming to improve dependency resolution and wrapper distribution speeds. By using these mirrors, we reduce the potential delays in accessing artifacts and Gradle resources, especially within China.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "kotlin-examples"
